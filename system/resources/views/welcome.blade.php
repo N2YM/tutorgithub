a50a3ecfb532b466724a6f2724e187908a2965bf
akmal002 <102295812+N2YM@users.noreply.github.com>
2023-04-05 20:04:05 +0700

    Update welcome.blade.php

--- system/resources/views/welcome.blade.php
+++ system/resources/views/welcome.blade.php
@@ -202,7 +202,7 @@
                             class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander </a>
+                        <a href="#" class="d-block">Admin  </a>
                     </div>
                 </div>
 
