8e642760c91eb18a859ac9a19f963a38237dae93
N2YM <102295812+N2YM@users.noreply.github.com>
2023-04-05 20:00:55 +0700

    test ubah nama side bar

--- system/resources/views/welcome.blade.php
+++ system/resources/views/welcome.blade.php
@@ -202,7 +202,7 @@
                             class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">Alexander </a>
                     </div>
                 </div>
 
